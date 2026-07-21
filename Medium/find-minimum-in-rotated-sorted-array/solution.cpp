fd482b6823e2efb86b078e6316dca9c06a71f53b
Vincenzo-sur <01trinetra10@gmail.com>
2026-07-21 17:23:25 +0530

    [Medium] Add solution for Find Minimum in Rotated Sorted Array

---
+++ Medium/find-minimum-in-rotated-sorted-array/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int lo = 0, hi = nums.size() - 1;
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (nums[lo] <= nums[mid] && nums[mid] <= nums[hi]) {
+                hi = mid;
+            } else if (nums[mid] > nums[hi]) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+
+        return nums[lo];
+    }
+};
