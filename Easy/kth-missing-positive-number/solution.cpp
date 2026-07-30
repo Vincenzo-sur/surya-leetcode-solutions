59a2d7185a473ff5e0e2d94f7a5db782786cb1b3
Vincenzo-sur <01trinetra10@gmail.com>
2026-07-30 17:28:31 +0530

    [Easy] Add solution for Kth Missing Positive Number

---
+++ Easy/kth-missing-positive-number/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int lo = 0, hi = arr.size() - 1;
+        while(lo <= hi){
+            int mid = (lo+hi)/2;
+            int miss = arr[mid] - mid - 1;
+            if(miss < k) lo = mid + 1;
+            else hi = mid - 1;
+        }
+        return k + 1 + hi;
+        
+    }
+};
