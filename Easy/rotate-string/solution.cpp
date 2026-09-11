9dfb9a828bcf3024ebdcc372f76cdd0559b6136b
Vincenzo-sur <01trinetra10@gmail.com>
2026-09-11 20:55:19 +0530

    [Easy] Add solution for Rotate String

---
+++ Easy/rotate-string/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n = s.size();
+
+        if (n != goal.size())
+            return false;
+
+        for (int shift = 0; shift < n; shift++) {
+            bool match = true;
+
+            for (int i = 0; i < n; i++) {
+                if (s[(i + shift) % n] != goal[i]) {
+                    match = false;
+                    break;
+                }
+            }
+
+            if (match)
+                return true;
+        }
+
+        return false;
+    }
+};
